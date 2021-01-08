54f97638cf7534f6d6b174bc1f289ae7de3705d2
Javier Saldarriaga <55507968+JavierSagah@users.noreply.github.com>
2021-01-07 19:57:57 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import pytest
 
 @pytest.mark.parametrize('value', [True, False])
-def test_my_test(value):
+def test_my_test1(value):
   assert value == True
- 
+
+def test_my_test2():
+  assert True == True
